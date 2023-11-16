ea94a37b5ac8b06df986539939af1e6abac3f41c
konrad_john@t-online.de <noLr^]h!%695Z1pB9I[N>
2023-11-16 23:33:52 +0100

    ranks fix

--- logic/strategy.py
+++ logic/strategy.py
@@ -82,7 +82,7 @@ def decide(table: Table) -> Bet:
     top_multi_rank, top_multi = find_top_multi(ranks)
     print(f'{top_multi_rank} x {top_multi}')
     print(f'folding?: {rankv[top_multi_rank] <= 10}')
-    if top_multi < 2 or rankv[top_multi_rank] <= 10:
+    if top_multi < 2 and rankv[top_multi_rank] <= 9:
         print('folding for low cards')
         return Bet(0)
 
