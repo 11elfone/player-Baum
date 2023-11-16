4ee44f350356bb44ef8f6030a3cd97551e1184f3
konrad_john@t-online.de <noLr^]h!%695Z1pB9I[N>
2023-11-16 20:06:40 +0100

    added restructure to dictionaries

--- logic/strategy.py
+++ logic/strategy.py
@@ -1,10 +1,46 @@
 from models.bet import Bet
 from models.table import Table
+from models.card import Card
+from models.suit import Suit
+
+
+def restructure(common: list[Card], hand: list[Card]) -> tuple[dict[str, int], dict[Suit, int]]:
+    ranks = {}
+    suits = {}
+    for c in hand:
+        if str(c.rank) in ranks.keys():
+            ranks[str(c.rank)] += 1
+        else:
+            ranks[str(c.rank)] = 1
+
+        if c.suit in suits.keys():
+            suits[c.suit] += 1
+        else:
+            suits[c.suit] = 1
+
+    for c in common:
+        if str(c.rank) in ranks.keys():
+            ranks[str(c.rank)] += 1
+        else:
+            ranks[str(c.rank)] = 1
+
+        if c.suit in suits.keys():
+            suits[c.suit] += 1
+        else:
+            suits[c.suit] = 1
+
+    return ranks, suits
 
 
 def decide(table: Table) -> Bet:
     # TODO: Add Poker Logic Here... :)
 
+    hand = table.players[table.activePlayer].cards
+    common = table.communityCards
+
+    ranks, suits = restructure(common, hand)
+
+    # go all-in
     bet = Bet(table.players[table.activePlayer].stack)
 
     return bet
