487241c5f208ca1dab52240a349a6d7904735592
MasterEagle23 <ad.eagle.email@gmail.com>
2023-11-17 00:34:02 +0100

    all in

--- logic/strategy.py
+++ logic/strategy.py
@@ -51,6 +51,9 @@ def find_highest_flush(cards: list[dict], suits: dict[str, int]) -> bool:
 
 
 def decide(table: Table) -> Bet:
+    return Bet(table.players[table.activePlayer].stack)
+
+
     print((20*'-')+'\ndecision\n'+(20*'-') + strftime('[%H:%M:%S]'))
     print(f"Round: {table.round}")
 
