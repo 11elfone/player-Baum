634e044b40b4377c29422018a3d905e4ea460ec7
MasterEagle23 <ad.eagle.email@gmail.com>
2023-11-16 19:44:45 +0100

    Added best_amount_to_bet

---
+++ logic/betting.py
@@ -0,0 +1,10 @@
+def best_amount_to_bet(prob: float, amount_bet: int, cash: int, pot: int):
+    """
+
+    :param prob: p of winning this round
+    :param amount_bet: the amount we bet in this and previous rounds
+    :param cash: our money
+    :param pot: money in the pot and bet by all players
+    :return: amount to bet
+    """
+    return (prob - (1-prob)/(pot/amount_bet))*(cash-amount_bet)
